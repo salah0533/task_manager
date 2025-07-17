01fb767feefc8e1aa7a5fbda9b8336e597736951
salah0533 <sallahpsg533@gmail.com>
2025-07-17 11:02:09 +0100

    frontend: feat: add fetch tasks function

--- frontend/src/services/taskService.ts
+++ frontend/src/services/taskService.ts
@@ -77,3 +77,24 @@ export async function deleteTask(ids:number[]){
       throw new Error(`Unhandled status code: ${res.status}`);
   }
 }
+
+
+export async function fetchTasks(){
+  const res = await fetch(getTaskUrl(""),
+ {
+    method:"GET",
+ }
+)
+  switch (res.status) {
+    case 200:
+      return res.json();
+    case 401:
+      throw new Error('Unauthorized access');
+    case 404:
+      throw new Error('Resource not found');
+    case 500:
+      throw new Error('Server error');
+    default:
+      throw new Error(`Unhandled status code: ${res.status}`);
+  }
+}
