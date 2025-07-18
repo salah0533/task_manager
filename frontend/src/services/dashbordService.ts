0fc5ffb713f99c131d24064d51c7284adc194f5e
salah0533 <sallahpsg533@gmail.com>
2025-07-18 17:44:02 +0100

    frontend: feat: create dashbordService file and add fetch statistic function to get stat data from the server

---
+++ frontend/src/services/dashbordService.ts
@@ -0,0 +1,19 @@
+import {getDashbordUrl} from "@/utils/urlHelpers"
+
+export async function fetch_stats(){
+    const res = await fetch(getDashbordUrl("/stats"))
+  switch (res.status) {
+    case 200:
+      return res.json();
+    case 401:
+      throw new Error('Unauthorized access');
+    case 404:
+      throw new Error('Resource not found');
+    case 500:
+      throw new Error('Server error');
+    default:
+      throw new Error(`Unhandled status code: ${res.status}`);
+  }
+
+}
+    
