f7fca1bcd0c837090fdb94af9227e866b11b9876
salah0533 <sallahpsg533@gmail.com>
2025-07-21 12:44:12 +0100

    frontend: feat: add urlhelper to generate auth api endpoint

--- frontend/src/utils/urlHelpers.ts
+++ frontend/src/utils/urlHelpers.ts
@@ -3,3 +3,4 @@ import {API_BASE_URL} from "../constants/configs"
 export const getApiUrl = (endpoint:string)=> `${API_BASE_URL}${endpoint}`
 export const getTaskUrl = (endpoint:string)=> `${API_BASE_URL}/task${endpoint}`
 export const getDashboardUrl = (endpoint:string)=> `${API_BASE_URL}/dashbord${endpoint}`
+export const getAuthUrl = (endpoint:string)=> `${API_BASE_URL}/auth${endpoint}`
