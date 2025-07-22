2d7ec3130520402ab728ae4ca69ce4e466be080e
salah0533 <sallahpsg533@gmail.com>
2025-07-22 18:08:16 +0100

    frontend: feat: create a tokenExist that check if the token exist

---
+++ frontend/src/utils/cokiesHelper.ts
@@ -0,0 +1,12 @@
+import { setLogIn,setLogOut } from "@/redux/slices/isLogedInSlice"
+
+export const tokenExist = (isLogedIn:boolean):boolean=>{
+    if(!isLogedIn){
+        const data = Boolean(localStorage.getItem("isLogedIn"))
+        if(data)
+            return true
+        else 
+            return false
+    }
+    return true
+}
