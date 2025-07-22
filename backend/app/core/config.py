6d81b535ca98152cfa8f6852022062a5d9af3924
salah0533 <sallahpsg533@gmail.com>
2025-07-22 20:30:16 +0100

    backend: feat: add cokie max age in config file

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,3 +2,4 @@ SECRET_KEY = "you-sekret-key"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60  
 ALGORITHM = "HS256"
 DATABASE_URL = "postgresql+psycopg2://postgres:0882%26%26533@localhost:5432/taskManager"
+COKIE_MAX_AGE = 30 * 24 * 60 * 60
