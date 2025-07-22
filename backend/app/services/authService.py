eb3c2e24cc613f5f6f61d0bdf3270bb36bdf189d
salah0533 <sallahpsg533@gmail.com>
2025-07-22 19:45:22 +0100

    backend: feat: add authentication service

---
+++ backend/app/services/authService.py
@@ -0,0 +1,50 @@
+from datetime import datetime,timedelta
+from fastapi import HTTPException
+from jose import JWTError,jwt
+from passlib.context import CryptContext
+from email_validator import validate_email
+from fastapi import Request, Depends
+from core.config import ACCESS_TOKEN_EXPIRE_MINUTES, ALGORITHM,SECRET_KEY
+
+
+
+def generate_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def check_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
+    
+
+def get_current_user(request: Request):
+    token = request.cookies.get("access_token")
+    if not token:
+        raise HTTPException(status_code=401, detail="No token provided")
+
+    payload = check_token(token)
+    if not payload:
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
+
+    return payload["id"]
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain, hashed):
+    return pwd_context.verify(plain, hashed)
+
+def valid_email(email:str):
+    try:
+        validate_email(email)
+        return True
+    except:
+        return False
