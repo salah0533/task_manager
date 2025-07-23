0e31dd88c7eff064075ff0fb566d56c29ed1e488
salah0533 <sallahpsg533@gmail.com>
2025-07-23 14:25:50 +0100

    backend: refactor: update logic for added task counter; add stats initialization on user registration

--- backend/app/services/stat_service.py
+++ backend/app/services/stat_service.py
@@ -6,25 +6,35 @@ from models.tasks import Task
 from models.task_status import TaskStatus
 from services.task_service import fetch_task_statuses
 
-def fetch_stats(db:Session):
+def fetch_stats(db:Session,user_id:int):
     try:
-        stat = {k:v for k,v in db.query(TaskStats).first().to_dict().items() }
+        stat = {k:v for k,v in db.query(TaskStats).filter(TaskStats.user_id==user_id).first().to_dict().items() }
+        stat["added_counter"] = db.query(Task).filter(Task.user_id==user_id).count()
+        
         status_counts = (
             db.query(TaskStatus.status, func.count(Task.id))
             .join(Task, Task.status_id == TaskStatus.id)
+            .filter(Task.user_id == user_id)
             .group_by(TaskStatus.status)
             .all()
         )
 
         status_counts = {status:count for status,count in status_counts}
         aviable_status ={status[0]:0 for status in  db.query(TaskStatus.status).all()}
-        
+
         stat.update(aviable_status)
         stat.update(status_counts)
-        
         del stat['id']
         return stat
     
     except SQLAlchemyError as e:
-        print(e)
+        raise e
+
+def add_init_stat(db:Session,user_id:int):
+    try:
+        new_stats = TaskStats(del_counter=0,edit_counter=0,user_id=user_id)
+        db.add(new_stats)
+        db.commit()
+    except SQLAlchemyError as e:
+        db.rollback()
         raise e
