29cd71f6ae99b7a0c3596afb355a899224bc1b56
salah0533 <sallahpsg533@gmail.com>
2025-07-23 14:14:52 +0100

    backend: refactor: change id to user_id in  palyload JWT also change user_exist name to user_exist_by_email

--- backend/app/routes/auth_routes.py
+++ backend/app/routes/auth_routes.py
@@ -3,8 +3,7 @@ from services.authService  import generate_token,hash_password,verify_password
 from schemas.authSchemas import LogInBase,RegisterBase
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
-from services.usersService import user_exist,add_user,get_user_by_email
-from models.users import Users
+from services.usersService import user_exist_by_email,add_user,get_user_by_email
 from services.authService import valid_email
 from db.session import get_db
 from core.config import COKIE_MAX_AGE
@@ -22,7 +21,7 @@ def login(req:LogInBase, response: Response,db: Session = Depends(get_db)):
         if not verify_password(req.password,user.hashedpass):
             raise HTTPException(status_code=401, detail="Invalid credentials")
 
-        token = generate_token({"id": user.id})
+        token = generate_token({"user_id": user.id})
         response.set_cookie(
             key="access_token",
             value=token,
@@ -41,14 +40,14 @@ def register(req: RegisterBase, db: Session = Depends(get_db)):
     if not valid_email(req.email):
         raise HTTPException(status_code=500,detail="not valid email")
     
-    existing_user = user_exist(db,req.email)
+    existing_user = user_exist_by_email(db,req.email)
     if existing_user:
         raise HTTPException(status_code=400, detail="Email already registered")
 
     hashpass = hash_password(req.password)
 
     try:
-        user = add_user(db,name =req.name,email=req.email,hashedpass=hashpass)
+        add_user(db,name =req.name,email=req.email,hashedpass=hashpass)
     except SQLAlchemyError as e:
         raise HTTPException(status_code=500,detail="couldn't register a new user")
 
