0a52e4f20f030d8720e0edafaa7bf8263e0e5c38
salah0533 <sallahpsg533@gmail.com>
2025-07-23 21:14:56 +0100

    backend: chore: init value of config file

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,7 +1,7 @@
 SECRET_KEY = "you-sekret-key"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60  
+ACCESS_TOKEN_EXPIRE_MINUTES = 30 * 24 * 60 * 60
 ALGORITHM = "HS256"
-DATABASE_URL = "postgresql+psycopg2://postgres:0882%26%26533@localhost:5432/taskManager"
+DATABASE_URL = ""
 COKIE_MAX_AGE = 30 * 24 * 60 * 60
 
 SERVER_HOST = "127.0.0.1"
