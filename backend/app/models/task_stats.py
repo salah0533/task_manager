c2d3f7cbc5b9472b21430ffd0b032dea19a72f28
salah0533 <sallahpsg533@gmail.com>
2025-07-23 14:16:50 +0100

    backend: refactor: remove the add_counter from TaskStats model

--- backend/app/models/task_stats.py
+++ backend/app/models/task_stats.py
@@ -1,19 +1,21 @@
 from db.base import Base
-from sqlalchemy import Column,Integer
+from sqlalchemy import Column,Integer,ForeignKey
+from sqlalchemy.orm import relationship
+
 
 
 class TaskStats(Base):
     __tablename__ = "task_stats"
 
-    id = Column(Integer,primary_key=True,default=1)
-    add_counter = Column(Integer,default=0)
+    id = Column(Integer,primary_key=True)
     del_counter   = Column(Integer,default=0)
     edit_counter  = Column(Integer,default=0)
+    user_id = Column(Integer,ForeignKey("users.id"),nullable=False)
+    user = relationship("Users")
 
     def to_dict(self):
         return {
             "id": self.id,
-            "added_counter": self.add_counter,
             "edit_counter": self.edit_counter,
             "delete_counter": self.del_counter,
         }
