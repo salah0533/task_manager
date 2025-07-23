45e837291c824838a93df13554ac8703c4fa9c43
salah0533 <sallahpsg533@gmail.com>
2025-07-23 14:33:42 +0100

    backend: feat: add auth routes and integrate uvicorn server

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
+import uvicorn
+from core.config import PORT,SERVER_HOST
 from db.session import engine
 from db.base import Base
 from routes.task_routes import router as task_routes
 from routes.dashbord_routes import router as dashbord_routes
 from fastapi.middleware.cors import CORSMiddleware
-
+from routes.auth_routes import router as auth_routes
 
 app = FastAPI()
 origins = [
@@ -22,4 +24,8 @@ app.add_middleware(
 
 app.include_router(task_routes)
 app.include_router(dashbord_routes)
+app.include_router(auth_routes)
 
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=SERVER_HOST, port=PORT)
