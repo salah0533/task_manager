388d6ec6d4b4e66dea537ec5b493a76c31eadbfb
salah0533 <sallahpsg533@gmail.com>
2025-07-23 14:32:34 +0100

    backend: refactor: update get_current_user to check user existence and return user

--- backend/app/services/authService.py
+++ backend/app/services/authService.py
@@ -3,7 +3,10 @@ from fastapi import HTTPException
 from jose import JWTError,jwt
 from passlib.context import CryptContext
 from email_validator import validate_email
+from services.usersService import get_user_by_id
+from sqlalchemy.orm import Session
 from fastapi import Request, Depends
+from db.session import get_db
 from core.config import ACCESS_TOKEN_EXPIRE_MINUTES, ALGORITHM,SECRET_KEY
 
 
@@ -23,17 +26,26 @@ def check_token(token: str):
         return None
     
 
-def get_current_user(request: Request):
+def get_current_user(request: Request, db: Session = Depends(get_db)):
     token = request.cookies.get("access_token")
     if not token:
         raise HTTPException(status_code=401, detail="No token provided")
+    try:
+        payload = check_token(token)
+        user_id = payload.get("user_id")
+        if not user_id:
+            raise HTTPException(status_code=401, detail="Invalid token payload")
 
-    payload = check_token(token)
-    if not payload:
+        user = get_user_by_id(db,user_id)
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
+
+        return user 
+    
+    except :
         raise HTTPException(status_code=401, detail="Invalid or expired token")
 
-    return payload["id"]
-
+"-----------------------"
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
